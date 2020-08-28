52f680209ce146b6c0c9fb4bd3d77b31a9d30ba3
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-28 11:04:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiou]/i)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,7 +11,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text_arr = text.grep(/\b\w{5}\b/i)
+  return text_arr
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
