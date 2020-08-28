a0912af4fbe5ed144a4f96d5e1a79ff761fae58d
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-28 12:15:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,12 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\b[a-z]+[a-z]\b/) && !text.match(/\b[A-Z].*[\.,:;!"']/)
-    return true
-  else
-    return false
-  end
-end
+
 
 def valid_phone_number?(phone)
  if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
