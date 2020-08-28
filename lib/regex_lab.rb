e1c42bbfb2dbfc39e294615c659beab645dd3993
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-28 11:54:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if word.match(/\b[A-Z].*[\.,:;!]/)
+  if word.match(/\b[A-Z].*[\.,:;!"']/)
     return true
   else
     return false
